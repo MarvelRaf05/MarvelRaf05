624a9b4263f8244f8e8b2b824f70d64d6a879176
MarvelRaf <raf.benneker@gmail.com>
2024-11-01 10:08:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// active hamburger menu 
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// rotate text JavaScript Code
+
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("");
